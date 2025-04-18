64e3188ef84574e985dc7f0fa3bebe4f8b9faa66
Dmytro <ddemin20052016@gmail.com>
2025-04-18 22:31:01 +0300

    add Form component + add basic styles

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,28 @@
 import { ref } from "vue";
 import { useFetchTracks } from "./composables/useFetchTracks";
 import TrackList from "./components/TrackList.vue";
+import TrackForm from "./components/TrackForm.vue";
 
 const page = ref(1);
 
 const { data, loading, error } = useFetchTracks({ page });
+const modalOpen = ref(false);
 </script>
 
 <template>
   <TrackList :tracks="data?.data" />
-  <!-- <div class="grid grid-cols-2"> -->
-  <button v-if="page < data?.meta.totalPages" @click="page++">+</button>
-  <button v-if="page !== 1" @click="page++">-</button>
-  <!-- </div> -->
+  <div class="grid grid-cols-2 gap-4">
+    <button v-if="page !== 1" class="col-start-1 bg-gray-500" @click="page--">
+      -
+    </button>
+    <button
+      v-if="page < data?.meta.totalPages"
+      class="col-start-2 bg-gray-500"
+      @click="page++"
+    >
+      +
+    </button>
+  </div>
+
+  <TrackForm v-if="modalOpen" />
 </template>
---
+++ src/components/TrackForm.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { Track } from "@/types";
+
+type NewTrack = {
+  title: "string";
+  artist: "string";
+  album: "string";
+  genres: ["string"];
+  coverImage: "string";
+};
+
+defineProps<{
+  initialData?: Track;
+}>();
+
+defineEmits<{
+  submit: [NewTrack];
+  close: [];
+}>();
+
+const handleSubmit = () => {};
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 w-full h-full bg-black/50 z-30"></div>
+
+  <div
+    class="fixed top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] w-full h-full flex items-center justify-center z-30"
+  >
+    <div class="max-w-[400px] max-h-[500px] bg-white rounded-md">
+      <form @submit.prevent="handleSubmit" class="grid grid-cols-2 gap-4 p-6">
+        <input class="px-4 py-2 border rounded-md" />
+        <input class="px-4 py-2 border rounded-md" />
+        <input class="px-4 py-2 border rounded-md" />
+        <select class="px-4 py-2 border rounded-md"></select>
+        <input class="px-4 py-2 border rounded-md" />
+
+        <div class="col-span-2 w-full flex gap-2">
+          <button class="flex-1" type="button">Cancel</button>
+          <button class="bg-black text-white flex-1" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
